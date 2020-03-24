7b4a6b3b6c3bccd121e5d2d2d467ae75bba582b4
Guilherme Carreiro <karreiro@gmail.com>
2020-03-24 17:33:01 -0300

    Introduction

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -6,10 +6,10 @@
   var title = 'Let\'s get started!';
   var content = '<p>Welcome to the <b>Learn DMN in 15 minutes</b> course!</p>' +
     '<img src="/assets/logo.png" />' +
-    '<p>If you notice any unclear example or section that deserves some polishment, please ' +
+    '<p>If you notice any unclear example or some possible enhacement, please ' +
     'report an issue <a href=\'https://github.com/karreiro/learn-dmn-in-15-minutes/issues\'>here</a>' +
     ', and we will fix it as soon as possible!</p>' +
-    '<p>Contributions to the source code are quite welcomed as well.</p>' +
+    '<p>Contributions are quite welcomed as well.</p>' +
     '<p>We hope you enjoy it! :)</p>';
 
   LEARN_DMN.Lights.turnLightsOff();
